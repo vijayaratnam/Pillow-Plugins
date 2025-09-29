6054f87b2c7cf511f4fbcc85a051475359310d28
59de44955ebd <59de44955ebd@gmail.com>
2025-09-29 17:34:53 +0200

    fixed FfmpegImagePlugin.py for Linux

--- FfmpegImagePlugin.py
+++ FfmpegImagePlugin.py
@@ -28,14 +28,15 @@ IS_WIN = os.name == 'nt'
 if IS_WIN:
     startupinfo = subprocess.STARTUPINFO()
     startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-
+    if ' ' in FFMPEG_BIN:
+        FFMPEG_BIN = f'"{FFMPEG_BIN}"'
 
 ########################################
 #
 ########################################
 def _open(fp, filename, ** kwargs):
     if os.path.splitext(filename)[1].lower() not in VIDEO_FORMATS:
-        raise SyntaxError('No FFmpeg file')
+        raise SyntaxError("No FFmpeg file")
 
     if IS_WIN:
         filename = os.path.realpath(filename)
@@ -43,12 +44,12 @@ def _open(fp, filename, ** kwargs):
     def load_frame(filename, frame):
         proc = subprocess.run(
             (
-                f'{os.path.basename(FFMPEG_BIN)} -hide_banner -v 0 -y -i "{filename}" '
-                f'-vf "select=eq(n\\,{frame})" -vframes 1 -c:v bmp -f image2pipe -'
+                f"{FFMPEG_BIN} -hide_banner -v 0 -y -i \"{filename}\" -vf select='eq(n\\,{frame})' "
+                f"-vframes 1 -c:v bmp -f image2pipe -"
             ),
-            cwd=os.path.dirname(FFMPEG_BIN),
-            startupinfo=startupinfo,
-            capture_output=True,
+            startupinfo = startupinfo if IS_WIN else None,
+            shell = not IS_WIN,
+            capture_output = True,
         )
         img = Image.open(io.BytesIO(proc.stdout), formats=("BMP",))
         img.load()
@@ -64,21 +65,20 @@ def _open(fp, filename, ** kwargs):
         proc = subprocess.run(
             (
                 'cmd.exe /cfor /f "usebackq tokens=2 delims= " %F IN '
-                f'(`{os.path.basename(FFMPEG_BIN)} -hide_banner -i "{filename}" -map 0:v:0 -c:v copy '
+                f'(`{FFMPEG_BIN} -hide_banner -i "{filename}" -map 0:v:0 -c:v copy '
                 '-f null -y nul 2^>^&1 ^|find "frame="`) do @echo %F'
             ),
-            cwd=os.path.dirname(FFMPEG_BIN),
-            capture_output=True,
-            startupinfo=startupinfo,
+            capture_output = True,
+            startupinfo = startupinfo,
         )
     else:
         proc = subprocess.run(
             (
-                f"{os.path.basename(FFMPEG_BIN)} -i \"{filename}\" -map 0:v:0 -c copy -f null -y /dev/null 2>&1 "
-                "| grep -Eo 'frame= *[0-9]+ *' | grep -Eo '[0-9]+' | tail -1"
+                f"{FFMPEG_BIN} -hide_banner -i \"{filename}\" -map 0:v:0 -c:v copy -f null -y /dev/null "
+                "2>&1 | grep -Eo 'frame= *[0-9]+ *' | grep -Eo '[0-9]+' | tail -1"
             ),
-            cwd=os.path.dirname(FFMPEG_BIN),
-            capture_output=True,
+            shell = True,
+            capture_output = True,
         )
 
     img.n_frames = int(proc.stdout.strip())
@@ -97,13 +97,8 @@ def _open(fp, filename, ** kwargs):
     img.tell = lambda: img.__frame
 
     # Image.show() calls dump with 'save_all': True, remove this
-#    img._dump = lambda d=img._dump, **kwargs: d(format="PNG", compress_level=1)
-
-    img._dump = (lambda file=None, format=None, d=img._dump, **options:
-            options.update({'save_all': False}) or d(file, format, **options))
-
-    # Make sure Image.sva() is never called with 'save_all': True
-#    img.save = lambda fp, format=None, s=img.save, **params: params.update({'save_all': False}) or s(fp, format, **params)
+    img._dump = (lambda file = None, format = None, d = img._dump, **options:
+            options.update({"save_all": False}) or d(file, format, **options))
 
     return img
 
@@ -117,4 +112,5 @@ Image.register_extensions(
 
 if __name__ == "__main__":
     img = Image.open("_test_files/test.mp4")
+    img.seek(123)
     img.show()
